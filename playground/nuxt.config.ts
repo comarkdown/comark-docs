604e0ff85e4c0c4767ab7dfad3a22851f90c5f6e
Sébastien Chopin <atinux@gmail.com>
2026-08-22 16:46:07 +0200

    docs: update url

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,13 +1,13 @@
 export default defineNuxtConfig({
   extends: ['..'],
   site: {
-    url: 'https://docs.comark.dev',
-    name: 'Comark Docs',
+    url: 'https://docs-template.comark.dev',
+    name: 'Comark Docs Template',
   },
   llms: {
-    domain: 'https://docs.comark.dev',
+    domain: 'https://docs-template.comark.dev',
     full: {
-      title: 'Full documentation',
+      title: 'Full documentation of Comark Docs Template',
       description: 'All pages concatenated in one file',
     },
   },
