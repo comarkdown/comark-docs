1b285bf9df95d65e1565c12176a0da6fd5b0b7a7
Sébastien Chopin <atinux@gmail.com>
2026-09-04 17:35:04 +0200

    fix: make sure to have payload not ISR to reuse cache disk

--- modules/config.ts
+++ modules/config.ts
@@ -174,6 +174,12 @@ export default defineNuxtModule<ComarkDocsOptions>({
         '/api/content/pr/*/search-sections': { isr },
         '/api/content/search-sections': { isr },
         '/api/code-explorer/**': { isr },
+        '/_payload.json': {
+          headers: { 'cache-control': `public, max-age=${isr}, s-maxage=${isr}, stale-while-revalidate=60` },
+        },
+        '/**/_payload.json': {
+          headers: { 'cache-control': `public, max-age=${isr}, s-maxage=${isr}, stale-while-revalidate=60` },
+        },
       }
 
       if (existsSync(contentPath)) {
